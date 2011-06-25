1833422c243da953fb908e351141e96d22c645af
Orange Dolphin <orange.dolphin3000@gmail.com>
2011-06-25 17:20:46 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '37633a92d38b572b73c1b0cad688c4b8435438d66698cfec6001a9d12c71a834e03fcfc2fd04a1e0577db45f0262b76b52b253e42ded3a52ccf849c046d1b7a3'
